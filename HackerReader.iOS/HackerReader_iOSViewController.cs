753023f5f9eff6f0e949cd53e5cddcdfca3917a1
Neil Kennedy <neil.p.kennedy@hotmail.com>
2014-03-25 22:24:32 +0000

    Simple Xamarin.iOS Hacker News Client
    Client to test HackerSharp library

---
+++ HackerReader.iOS/HackerReader_iOSViewController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.Dialog;
+using HackerSharpAPI;
+
+namespace HackerReader.iOS
+{
+	public partial class HackerReader_iOSViewController : DialogViewController
+	{
+		public HackerReader_iOSViewController () : base(UITableViewStyle.Plain, new RootElement("Hacker News"))
+		{
+		}
+
+		public async override void LoadView ()
+		{
+			base.LoadView ();
+
+			var news = await new HackerSharp().News();
+
+			Root.Add (new Section ());
+
+			foreach (var item in news) {
+				Root [0].Add (new StyledStringElement (item.Title, delegate {
+					NavigationController.PushViewController(new WebViewController(item.URL), true);
+				}));
+			}
+		}
+	}
+}
---
+++ HackerReader.iOS/WebViewController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using ReadSharp;
+
+namespace HackerReader.iOS
+{
+	public partial class WebViewController : UIViewController
+	{
+		string _url;
+
+		public WebViewController (string url) : base ()
+		{
+			_url = url;
+		}
+
+		public async override void LoadView ()
+		{
+			base.LoadView ();
+
+			Reader reader = new Reader();
+			Article article;
+
+			try
+			{
+				article = await reader.Read(new Uri(_url));
+				var webView = new UIWebView (View.Frame);
+				webView.LoadHtmlString(article.Content, new NSUrl(_url));
+
+				Add(webView);
+			}
+			catch (ReadException exc)
+			{
+				// handle exception
+			}
+
+		}
+	}
+}
+
